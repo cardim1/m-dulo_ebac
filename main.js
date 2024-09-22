038e194435449fd0a811dc444c069ec2550bf8df
Gabriel Cardim <159202927+cardim1@users.noreply.github.com>
2024-09-22 15:32:00 -0300

    Add files via upload
    movimentando aquivos para branch correta

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
-const campoA = document.getElementById('campoA')
-const campoB = document.getElementById('campoB')
-const formulario = document.getElementById('formulario')
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
 
-
-
-function validaForm() {
-    if (campoB.value > campoA.value) {
-        return true
-    } else {
-        return false
-    }
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
 }
 
-const container = document.getElementById('container')
-const novoParagrafo = document.createElement('p')
-novoParagrafo.innerHTML = ''
+form.addEventListener('submit', function (e) {
+    let formEValido = false;
+    e.preventDefault();
 
-formulario.addEventListener('submit', function (e) {
-    e.preventDefault()
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
 
-    if (validaForm()) {
-        novoParagrafo.textContent = 'Its valid, congratulations!'
-        container.appendChild(novoParagrafo)
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        const containerMensagemSucesso = document.querySelector('.sucess-message')
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
     } else {
-        novoParagrafo.textContent = 'Not valid. B field does not a bigger than A field'
-        container.appendChild(novoParagrafo)
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     }
-
-
 })
+
+nomeBeneficiario.addEventListener('keyup', function (e) {
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value);
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('erro');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
